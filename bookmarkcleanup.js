07f9963cfd0dfa3d094419c88692344b86f12f26
Steven G. Harms <steven@devbootcamp.com>
2015-01-03 16:18:53 -0500

    Remove jQuery: extend()

--- bookmarkcleanup.js
+++ bookmarkcleanup.js
@@ -1,5 +1,15 @@
+noQuery = {
+  extend: function(recipient, source) {
+    for (var key in source) {
+      if(source.hasOwnProperty(key)){
+        recipient[key] = source[key];
+      }
+    }
+  }
+};
+
 function Bookmark(data) {
-  $.extend(this, data);
+  noQuery.extend(this, data);
 
   this._resolveValidityState();
 }
@@ -56,7 +66,7 @@ Bookmark.prototype = {
 }
 
 function Container(data) {
-  $.extend(this, data);
+  noQuery.extend(this, data);
   this.raw = data;
   this.children = this._calculateChildren();
 }
